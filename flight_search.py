5c19943f979071350b28730faaf163249dfda84b
Reyner Veliz <reyner@reynerveliz.com>
2024-10-15 06:18:45 -0600

    Update empty iataCodes in sheety with TESTING

---
+++ flight_search.py
@@ -0,0 +1,4 @@
+class FlightSearch:
+    def get_destination_code(self, city_name):
+        code = "TESTING"
+        return code
--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from data_manager import DataManager
+from flight_search import FlightSearch
 
 
 data_manager = DataManager()
+flight_search = FlightSearch()
+
 sheet_data = data_manager.get_destination_data()
 # print(sheet_data)
+
+for row in sheet_data:
+    if row['iataCode'] == "":
+        row['iataCode'] = flight_search.get_destination_code(row['city'])
+
+data_manager.destination_data = sheet_data
+print(data_manager.destination_data)
